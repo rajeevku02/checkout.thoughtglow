bbd7bf82d83b206d5c5d9ac29d89dbe73e85af2a
Rajeev Kumar <rajeevku02@gmail.com>
2024-06-27 17:26:40 +0530

    Update

---
+++ _next/static/k0xXJZSoCvEYSh8CJWvt7/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/pages/index-5a26c5fc13152989.js"],"/404":["static/chunks/pages/404-b4439111801b0567.js"],"/_error":["static/chunks/pages/_error-5a00309fd5f4b49e.js"],sortedPages:["/","/404","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
